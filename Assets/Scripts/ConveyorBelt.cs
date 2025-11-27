3ea8e66d54358bac567832be36997871f6912000
rapsealk <piono623@naver.com>
2025-11-28 02:03:10 +0900

    feat: Remove cone

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ConveyorBelt : MonoBehaviour
 {
     [SerializeField] private float conveyorSpeed;
 
     private Material material;
+
+    [SerializeField]
+    private List<GameObject> onBelt;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,5 +20,22 @@ public class ConveyorBelt : MonoBehaviour
     void Update()
     {
         material.mainTextureOffset += new Vector2(0, 1) * conveyorSpeed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredVelocity = Vector3.right * conveyorSpeed * 10;
+
+        foreach (GameObject obj in onBelt)
+        {
+            var rigidbody = obj.GetComponent<Rigidbody>();
+
+            if (rigidbody != null)
+            {
+                Vector3 velocityChange = desiredVelocity - rigidbody.linearVelocity;
+                rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+            }
+            // obj.GetComponent<Rigidbody>().AddForce(Vector3.right * conveyorSpeed * 10 * 50, ForceMode.Force);
+        }
     }
 }
